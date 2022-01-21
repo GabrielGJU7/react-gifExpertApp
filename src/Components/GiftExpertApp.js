75d701ec9869053bbe81fc75a54c3c94660a9b22
Gabriel Jacobo <gjacoboulloa@gmail.com>
2022-01-21 13:24:08 -0600

    First commit

---
+++ src/Components/GiftExpertApp.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GiftExpertApp = () => {
+
+    
+    const [categories, setCategories] = useState(['Dragon ball']);
+
+
+  return <div>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories = {setCategories} />
+      <hr />
+       <ol>
+           {
+               categories.map( category => 
+               <GifGrid 
+                    category = { category } 
+                    key = { category }
+                    
+                /> )
+           }
+       </ol>
+
+  </div>;
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import { GiftExpertApp } from './Components/GiftExpertApp';
+
+ReactDOM.render(
+    <GiftExpertApp />,
+  document.getElementById('root')
+);
+
